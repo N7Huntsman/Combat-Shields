238bb9fb4b5e49b6dcebd39069ac42e2bcf0ecb7
Mlie <emipa606@users.noreply.github.com>
2023-06-01 21:57:26 +0200

    Updated for 1.4

--- Source/MedievalShields/ColorableShield.cs
+++ Source/MedievalShields/ColorableShield.cs
@@ -5,105 +5,94 @@ using UnityEngine;
 using Verse;
 using Random = System.Random;
 
-namespace CombatShields
+namespace CombatShields;
+
+public class ColorableShield : Apparel_Shield
 {
-    public class ColorableShield : Apparel_Shield
+    public override bool CheckPreAbsorbDamage(DamageInfo dinfo)
     {
-        public override bool CheckPreAbsorbDamage(DamageInfo dinfo)
+        if (dinfo.Instigator == null)
         {
-            if (dinfo.Instigator == null)
-            {
-                return false;
-            }
-
-            var melee = Wearer.skills.GetSkill(SkillDefOf.Melee);
-            var random = new Random();
-            var chance = random.Next(0, 21);
-            // this.AbsorbedDamage(dinfo);
-
-            // full hit
-
-            // partial hit
-
-            // partial block
-
-            // full block
-
-
-            if (chance >= melee.levelInt)
-            {
-                var i = (int)dinfo.Amount;
-                i /= 4;
-
-                HitPoints -= i;
-            }
-            else
-            {
-                var i = (int)dinfo.Amount;
-                i /= 10;
-
-                HitPoints -= i;
-            }
-
             return false;
         }
 
+        var melee = Wearer.skills.GetSkill(SkillDefOf.Melee);
+        var random = new Random();
+        var chance = random.Next(0, 21);
 
-        [DebuggerHidden]
-        public override IEnumerable<Gizmo> GetWornGizmos()
+        if (chance >= melee.levelInt)
         {
-            yield return new Gizmo_ShieldStatus
-            {
-                shield = this
-            };
+            var i = (int)dinfo.Amount;
+            i /= 4;
+
+            HitPoints -= i;
+        }
+        else
+        {
+            var i = (int)dinfo.Amount;
+            i /= 10;
+
+            HitPoints -= i;
         }
 
-        public override void DrawWornExtras()
+        return false;
+    }
+
+
+    [DebuggerHidden]
+    public override IEnumerable<Gizmo> GetWornGizmos()
+    {
+        yield return new Gizmo_ShieldStatus
         {
-            base.DrawWornExtras();
+            shield = this
+        };
+    }
 
-            if (!ShouldDisplay)
-            {
-                return;
-            }
+    public override void DrawWornExtras()
+    {
+        base.DrawWornExtras();
 
-            var num = 0f;
-            var vector = Wearer.Drawer.DrawPos;
-            vector.y = AltitudeLayer.Pawn.AltitudeFor();
-            var s = new Vector3(1f, 1f, 1f);
-            if (Wearer.Rotation == Rot4.North)
-            {
-                vector.y = AltitudeLayer.Item.AltitudeFor();
-                vector.x -= 0.2f;
-                vector.z -= 0.2f;
-            }
-            else if (Wearer.Rotation == Rot4.South)
-            {
-                vector.y += 0.033f;
-                vector.x += 0.2f;
-                vector.z -= 0.2f;
-            }
-            else if (Wearer.Rotation == Rot4.East)
-            {
-                vector.z -= 0.2f;
-                num = 90f;
-            }
-            else if (Wearer.Rotation == Rot4.West)
-            {
-                vector.y = AltitudeLayer.MoteOverhead.AltitudeFor();
-                vector.z -= 0.2f;
-                num = 270f;
-            }
-
-            if (shieldMat == null)
-            {
-                shieldMat = MaterialPool.MatFrom(def.graphicData.texPath);
-            }
-
-            shieldMat.color = Stuff.stuffProps.color;
-            var matrix = default(Matrix4x4);
-            matrix.SetTRS(vector, Quaternion.AngleAxis(num, Vector3.up), s);
-            Graphics.DrawMesh(MeshPool.plane10, matrix, shieldMat, 0);
+        if (!ShouldDisplay)
+        {
+            return;
         }
+
+        var num = 0f;
+        var vector = Wearer.Drawer.DrawPos;
+        vector.y = AltitudeLayer.Pawn.AltitudeFor();
+        var s = new Vector3(1f, 1f, 1f);
+        if (Wearer.Rotation == Rot4.North)
+        {
+            vector.y = AltitudeLayer.Item.AltitudeFor();
+            vector.x -= 0.2f;
+            vector.z -= 0.2f;
+        }
+        else if (Wearer.Rotation == Rot4.South)
+        {
+            vector.y += 0.033f;
+            vector.x += 0.2f;
+            vector.z -= 0.2f;
+        }
+        else if (Wearer.Rotation == Rot4.East)
+        {
+            vector.z -= 0.2f;
+            num = 90f;
+        }
+        else if (Wearer.Rotation == Rot4.West)
+        {
+            vector.y = AltitudeLayer.MoteOverhead.AltitudeFor();
+            vector.z -= 0.2f;
+            num = 270f;
+        }
+
+        if (shieldMat == null)
+        {
+            shieldMat = MaterialPool.MatFrom(def.graphicData.texPath);
+        }
+
+        shieldMat.color = Stuff.stuffProps.color;
+        var matrix = default(Matrix4x4);
+        matrix.SetTRS(vector, Quaternion.AngleAxis(num, Vector3.up), s);
+        Graphics.DrawMesh(MeshPool.plane10, matrix, shieldMat, 0);
     }
 }
